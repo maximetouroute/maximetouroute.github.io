69c8f2702da699324e5652ea8e193444d9cc54ce
maximetouroute <maxime.touroute@gmail.com>
2019-10-03 20:26:17 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9a895c487b08cb7cf878.js"
+    "url": "webpack-runtime-cd97e50f0450345db975.js"
   },
   {
     "url": "app-53c0e998c370187c080c.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ef1d0bb02d1fca0494d74b47a97b9da6"
+    "revision": "120d18954c3defdc58979919d9020fe3"
   },
   {
     "url": "component---src-pages-404-js.dc64ed3707115e98ad00.css"
