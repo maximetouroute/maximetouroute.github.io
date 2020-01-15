12ca4abace74ec0457a977b077d93181d634c904
Maxime Touroute <maxime.touroute@gmail.com>
2020-01-15 21:59:29 +0100

    Fix: project images were not true links

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,70 +9,80 @@ export default function Index({ data }) {
 
   return (
     <MainLayout>
-    <MetaTags title={'Home'}/>
+      <MetaTags title={'Home'} />
       <div className="home homePosts">
         <div className="postGrid">
           {posts
             .filter(post => post.node.frontmatter.title.length > 0)
             .map(({ node: post }) => {
               return (
-
-                <div className="post" onClick={() => {
-                  navigate(post.frontmatter.path + '#content')
-                }} key={post.id}>
-                  <div className="postHeader" style={{
-                    backgroundImage: 'url(' + post.frontmatter.image.childImageSharp.fixed.src + ')',
-                  }}>
+                // TODO : remove underline and otherwise seems ok
+                <Link
+                  className="post"
+                  to={`${post.frontmatter.path}#content`}
+                  key={post.id}
+                >
+                  <div
+                    className="postHeader"
+                    style={{
+                      backgroundImage:
+                        'url(' +
+                        post.frontmatter.image.childImageSharp.fixed.src +
+                        ')',
+                    }}
+                  >
                     <div className="inside">
-                      <span className="postTitle">{post.frontmatter.title}</span>
-                      <span className="post-meta postTag">{post.frontmatter.subtitle}</span>
+                      <span className="postTitle">
+                        {post.frontmatter.title}
+                      </span>
+                      <span className="post-meta postTag">
+                        {post.frontmatter.subtitle}
+                      </span>
                     </div>
                   </div>
-                </div>
+                </Link>
               )
             })}
-
         </div>
 
         <div className="moreProjects">
           <Link to="/about">About me</Link>
         </div>
-
       </div>
     </MainLayout>
   )
 }
 
-  export const portfolioPostsQuery = graphql`
-      query IndexQuery {
-          allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }
-              filter: { frontmatter:  { category: { eq:"video"}}}
-          ) {
-              edges {
-                  node {
-                      excerpt(pruneLength: 250)
-                      id
-                      frontmatter {
-                          title
-                          date(formatString: "MMMM DD, YYYY")
-                          path
-                          category
-                          subtitle
-                          image {
-                              childImageSharp {
-                                  # Other options include height (set both width and height to crop),
-                                  # grayscale, duotone, rotate, etc.
-                                  fixed(width: 700) {
-                                      # Choose either the fragment including a small base64ed image, a traced placeholder SVG, or one without.
-                                      ...GatsbyImageSharpFixed
-                                      src
-                                  }
-
-                              }
-                          }
-                      }
-                  }
+export const portfolioPostsQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { category: { eq: "video" } } }
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 250)
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+            category
+            subtitle
+            image {
+              childImageSharp {
+                # Other options include height (set both width and height to crop),
+                # grayscale, duotone, rotate, etc.
+                fixed(width: 700) {
+                  # Choose either the fragment including a small base64ed image, a traced placeholder SVG, or one without.
+                  ...GatsbyImageSharpFixed
+                  src
+                }
               }
+            }
           }
-      }`
-
+        }
+      }
+    }
+  }
+`
