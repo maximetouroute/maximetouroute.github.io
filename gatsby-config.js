49cf184146599d2f6550bc659d9f8a0963895ad0
Maxime Touroute <maxime.touroute@gmail.com>
2020-01-29 17:25:49 +0100

    minor: update progressbar color

--- gatsby-config.js
+++ gatsby-config.js
@@ -110,7 +110,7 @@ module.exports = {
       resolve: `gatsby-plugin-nprogress`,
       options: {
         // Setting a color is optional.
-        color: `blue`,
+        color: `#3568cf`,
         // Disable the loading spinner.
         showSpinner: false,
       },
