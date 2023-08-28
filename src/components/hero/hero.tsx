41648b9747bad94496a71af509f46916c51ea6f6
Maxime <maxime.touroute@gmail.com>
2023-08-28 11:30:45 +0200

    New : hero component POC

---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,15 @@
+import { useTheme } from '@mui/styles';
+import { detailsCSS, grettingCSS, heroCSS, punchlineCSS, textCSS } from './styles';
+
+
+export default function Hero() {
+  const theme = useTheme();
+  return (
+    <div css={heroCSS}>
+        <div css={grettingCSS}>Hello 👋 </div>
+        <div css={punchlineCSS}>I'm Maxime</div>
+        <div css={detailsCSS}>One more line</div>
+        <p css={textCSS}>I work with cultural institutions and digital artists to create stuff</p>
+    </div>
+  );
+}
---
+++ src/components/hero/styles.ts
@@ -0,0 +1,35 @@
+import { CSSObject } from "@emotion/react";
+
+export const heroCSS:CSSObject = {
+
+    display: 'flex',
+flexDirection: 'column',
+justifyContent: 'center',
+minHeight: '50vh',
+fontWeight: 600,
+textAlign: 'center',
+};
+
+export const grettingCSS: CSSObject = {
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'flex',
+    fontSize: '2.625rem',
+    lineHeight: '4rem',
+};
+
+export const punchlineCSS: CSSObject = {
+    fontSize: '2.625rem',
+lineHeight: '4rem',
+marginBottom: 0
+};
+
+export const detailsCSS: CSSObject = {
+    fontSize: '2rem',
+    lineHeight: '3rem' 
+};
+
+export const textCSS: CSSObject = {
+    fontSize: '1.125rem',
+marginBottom: '2rem',
+};
