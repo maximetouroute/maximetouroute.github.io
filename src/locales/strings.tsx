6593bfb112502d5a3886913513f4eca890ad7384
Maxime <maxime.touroute@gmail.com>
2023-09-01 09:18:32 +0200

    New : small fixes

--- src/locales/strings.tsx
+++ src/locales/strings.tsx
@@ -224,17 +224,17 @@ export const educationPageStrings = stringBuilderEnFr([
   StringEnFr(
     'getApp',
     'Download the app on iPhone and Android',
-    `Télécharge l'appli sur iPhone et Android`
+    `Téléchargez l'appli sur iPhone et Android`
   ),
   StringEnFr(
     `teacherMode`,
-    `Access participants' creations directly on the app`,
-    `Déroulez vos ateliers et accédez au travail des élèves depuis l'appli.`
+    `Keep an eye on participants' creations directly on your account`,
+    `Déroulez vos ateliers et accédez au travail des élèves depuis l'appli`
   ),
   StringEnFr(
-    `Aucune contrainte d'espace, de matériel ou d'éclairage pour commencer à créer.`,
-    `Aucune contrainte d'espace, de matériel ou d'éclairage pour commencer à créer.`,
-    `Aucune contrainte d'espace, de matériel ou d'éclairage pour commencer à créer.`
+    `Aucune contrainte d'espace, de matériel ou d'éclairage pour commencer à créer`,
+    `Aucune contrainte d'espace, de matériel ou d'éclairage pour commencer à créer`,
+    `Aucune contrainte d'espace, de matériel ou d'éclairage pour commencer à créer`
   ),
   StringEnFr(
     `Mise en place facile`,
@@ -243,31 +243,31 @@ export const educationPageStrings = stringBuilderEnFr([
   ),
   StringEnFr(
     `
-  L'appli fonctionne sur tous les téléphones gratuitement. Les participants peuvent s'emparer de l'outil pendant, et après l'atelier.`,
+  L'appli fonctionne sur tous les téléphones gratuitement. Les participants peuvent s'emparer de l'outil pendant, et après l'atelier`,
     `
-  L'appli fonctionne sur tous les téléphones gratuitement. Les participants peuvent s'emparer de l'outil pendant, et après l'atelier.`,
+  L'appli fonctionne sur tous les téléphones gratuitement. Les participants peuvent s'emparer de l'outil pendant, et après l'atelier`,
     `
-  L'appli fonctionne sur tous les téléphones gratuitement. Les participants peuvent s'emparer de l'outil pendant, et après l'atelier.`
+  L'appli fonctionne sur tous les téléphones gratuitement. Les participants peuvent s'emparer de l'outil pendant, et après l'atelier`
   ),
   StringEnFr(
-    `Un smartphone et une appli, c'est tout ! Les participants peuvent créer de A à Z sans être bloqués techniquement.
+    `Un smartphone et une appli, c'est tout ! Les participants peuvent créer de A à Z sans être bloqués techniquement
   `,
-    `Un smartphone et une appli, c'est tout ! Les participants peuvent créer de A à Z sans être bloqués techniquement.
+    `Un smartphone et une appli, c'est tout ! Les participants peuvent créer de A à Z sans être bloqués techniquement
   `,
-    `Un smartphone et une appli, c'est tout ! Les participants peuvent créer de A à Z sans être bloqués techniquement.
+    `Un smartphone et une appli, c'est tout ! Les participants peuvent créer de A à Z sans être bloqués techniquement
   `
   ),
   StringEnFr(`Flow Rapide`, `Fast Creation`, `Flow Rapide`),
   StringEnFr(
     '5mns',
-    `5 minutes is enough to create your first content. That's more time available to test, experiment, and try again.`,
-    `5 minutes suffisent pour créer une scène. C'est du temps gagné pour tester, explorer, recommencer.`
+    `5 minutes is enough to create your first content. That's more time available to test, experiment, and try again`,
+    `5 minutes suffisent pour créer une scène. C'est du temps gagné pour tester, explorer, recommencer`
   ),
   StringEnFr(`Restitution simple`, `Restitution simple`, `Restitution simple`),
   StringEnFr(
     `restitution`,
-    `Capture and export videos of your creations so it's easy to share.`,
-    `Récupérez les créations en vidéo, diffusables sur toutes les plateformes existantes.`
+    `Capture and export videos of your creations so it's easy to share`,
+    `Récupérez les créations en vidéo, diffusables sur toutes les plateformes existantes`
   ),
   StringEnFr(
     `Accessible à la maison`,
@@ -284,8 +284,8 @@ export const educationPageStrings = stringBuilderEnFr([
   StringEnFr('Simple, Intuitif', `Easy to use`, `Simple, Intuitif`),
   StringEnFr(
     `smartApp`,
-    `A phone, an app and that's it! Participants can create from A to Z idependently`,
-    `Un smartphone et une appli, c'est tout ! Les participants peuvent créer de A à Z sans être bloqués techniquement.`
+    `A phone, an app and that's it! Participants can create from A to Z independently`,
+    `Un smartphone et une appli, c'est tout ! Les participants peuvent créer de A à Z sans être bloqués techniquement`
   ),
   StringEnFr('seoTitle', 'Subscription Confirmed', 'Inscription confirmée'),
   StringEnFr('divTitle', 'One more step...', `Encore une étape...`),
@@ -293,7 +293,7 @@ export const educationPageStrings = stringBuilderEnFr([
   StringEnFr(
     `athome`,
     `The app is free and works on every phone. Participants can keep creating after class`,
-    `L’appli fonctionne sur tous les téléphones gratuitement. Les participants peuvent s’emparer de l’outil pendant, et après l’atelier.`
+    `L’appli fonctionne sur tous les téléphones gratuitement. Les participants peuvent s’emparer de l’outil pendant, et après l’atelier`
   ),
   StringEnFr(
     'divSubtitle',
@@ -303,7 +303,7 @@ export const educationPageStrings = stringBuilderEnFr([
   StringEnFr(
     `easySetUp`,
     `You don't need a lot of space, expensive equipment or lighting to start creating`,
-    `Aucune contrainte d’espace, de matériel ou d'éclairage pour commencer à créer.`
+    `Aucune contrainte d’espace, de matériel ou d'éclairage pour commencer à créer`
   ),
   StringEnFr('goBackHome', 'Go back Home', `Retour Accueil`),
 ])
